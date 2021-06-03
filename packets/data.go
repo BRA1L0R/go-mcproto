6b35d075386da8a562650f3010152f7ba0bb8c0d
Pietro Tamilia <kapakapa197@gmail.com>
2021-06-03 12:16:05 +0200

    removed unused functions

--- packets/data.go
+++ packets/data.go
@@ -1,37 +1,9 @@
 package packets
 
 import (
-	"bytes"
 	"errors"
 	"reflect"
-
-	"github.com/BRA1L0R/go-mcproto/packets/serialization"
 )
-
-type standardPacket struct {
-	Data   *bytes.Buffer
-	Length int32
-
-	PacketID int32
-}
-
-func (p *standardPacket) SerializeData(inter interface{}) error {
-	val, err := getReflectValue(inter)
-	if err != nil {
-		return err
-	}
-
-	return serialization.SerializeFields(val, p.Data)
-}
-
-func (p *standardPacket) DeserializeData(inter interface{}) error {
-	val, err := getReflectValue(inter)
-	if err != nil {
-		return err
-	}
-
-	return serialization.DeserializeFields(val, p.Data)
-}
 
 func getReflectValue(inter interface{}) (val reflect.Value, err error) {
 	v := reflect.ValueOf(inter)
@@ -43,8 +15,4 @@ func getReflectValue(inter interface{}) (val reflect.Value, err error) {
 
 	val = v.Elem()
 	return
-}
-
-func (p *standardPacket) InitializePacket() {
-	p.Data = bytes.NewBuffer([]byte{})
 }
